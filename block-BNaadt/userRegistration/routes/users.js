c6d8547ff352840be85d838dbbd9e12b8324f1b6
Kunal <mailto.guptakunal@gmail.com>
2022-10-29 16:50:03 +0530

    finish

--- block-BNaadt/userRegistration/routes/users.js
+++ block-BNaadt/userRegistration/routes/users.js
@@ -1,28 +1,26 @@
 var express = require('express');
-const user = require('../models/user');
 var router = express.Router();
 var User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  console.log(req.session);
+  //console.log(req.session);
   res.render('users');
 });
 
 router.get('/register', function (req, res, next) {
-  var error = req.flash('error')[0];
-  res.render('register', { error: error });
+  res.render('register', { error: req.flash('error')[0] });
 });
 
 router.post('/register', (req, res, next) => {
   User.create(req.body, (err, user) => {
     if (err) {
       if (err.code === 11000) {
-        req.flash('error', 'Add a unique email!');
+        req.flash('error', 'Email has been taken!');
         return res.redirect('/users/register');
       }
-      if (err.name === 'ValidatorError') {
-        req.flash('error', 'err.message');
+      if (err.name === 'ValidationError') {
+        req.flash('error', err.message);
         return res.redirect('/users/register');
       }
       return res.json({ err });
@@ -33,40 +31,38 @@ router.post('/register', (req, res, next) => {
 
 router.get('/login', function (req, res, next) {
   var error = req.flash('error')[0];
-  console.log(error);
-  res.render('login', { error: error });
+  //console.log(error);
+  res.render('login', { error });
 });
 
-router.post('/login', function (req, res, next) {
+router.post('/login', (req, res, next) => {
   var { email, password } = req.body;
   if (!email || !password) {
-    req.flash('error', 'Email/Password required');
+    req.flash('error', 'Email/Password required!');
     return res.redirect('/users/login');
   }
   User.findOne({ email }, (err, user) => {
     if (err) return next(err);
-    //no user
     if (!user) {
-      req.flash('error', 'Email is not  valid');
+      req.flash('error', 'Email is not registered');
       return res.redirect('/users/login');
     }
-    //user then compare password
     user.verifyPassword(password, (err, result) => {
       if (err) return next(err);
       if (!result) {
-        req.flash('error', 'Incorrect Password');
+        req.flash('error', 'Invalid password!');
         return res.redirect('/users/login');
       }
-      //persist logged in user information
       req.session.userId = user.id;
       res.redirect('/users');
     });
   });
 });
-//logout router
-router.get('/logout', (req, res) => {
+
+router.get('/logout', (req, res, next) => {
   req.session.destroy();
   res.clearCookie('connect.sid');
   res.redirect('/users/login');
 });
+
 module.exports = router;
